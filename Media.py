47dc4041a12fee1761add08ecf2fdb2e476cc7f3
yandorneles <yandorneles05@gmail.com>
2024-09-17 10:37:44 -0300

    Atividade media

---
+++ Media.py
@@ -0,0 +1,8 @@
+num1 = float(input("Informe o primeiro número:"))
+num2 = float(input("Informe o segundo número:"))
+num3 = float(input("Informe o terceiro número:"))
+num4 = float(input("Informe o quarto número:"))
+
+x = (num1 + num2 + num3 + num4)/4 
+
+print("A média é:"+ str(x))
