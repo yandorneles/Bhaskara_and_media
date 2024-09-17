1800b740c79548c98fb010a1251eab2883b65b18
yandorneles <yandorneles05@gmail.com>
2024-09-17 11:46:19 -0300

    Adicionando area

---
+++ Area.py
@@ -0,0 +1,26 @@
+#área do retângulo
+
+b = float(input("Informe o valor da base do retângulo:"))
+h = float(input("Informe o valor da área retângulo:"))
+
+a = b * h
+
+print(f"O valor da área do retângulo é: {a}")
+
+#área do triângulo
+
+b = float(input("Informe o valor da base do triângulo:"))
+h = float(input("Informe o valor da área do triângulo:"))
+
+a = (b * h)/2
+
+print(f"O valor da área triângulo é: {a}")
+
+#área do circulo
+
+r = float(input("Informe o valor do raio do circulo:"))
+
+a = 3.14 * r
+
+print(f"O valor da área do circulo é: {a}")
+
